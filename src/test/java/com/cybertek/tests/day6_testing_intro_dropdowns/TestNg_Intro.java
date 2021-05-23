9a8adde09a4b699e7f18f336e19d4119fec848d6
Ramin <raminrocker75@gmail.com>
2021-05-23 14:38:59 -0500

    Initial commit

--- src/test/java/com/cybertek/tests/day6_testing_intro_dropdowns/TestNg_Intro.java
+++ src/test/java/com/cybertek/tests/day6_testing_intro_dropdowns/TestNg_Intro.java
@@ -1,4 +1,36 @@
 package com.cybertek.tests.day6_testing_intro_dropdowns;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class TestNg_Intro {
+
+    @BeforeClass
+    public void setupClass(){
+        System.out.println("---> Before Class is running...");
+    }
+
+    @BeforeMethod // before method runs before each test
+    public void setupMethod(){
+        System.out.println("--> Before method is running...");
+    }
+
+    @AfterMethod // after method runs after each test
+    public void tearDownMethod(){
+        System.out.println("-->After method is running...");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("Running Test1...");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Running Test2...");
+    }
+
+
 }
